9ee6610a949b1d44d76f4b8b8531219771942824
jerod876 <31867382+jerod876@users.noreply.github.com>
2018-08-17 19:13:30 -0400

    first upload
    practice1

---
+++ javas.js
@@ -0,0 +1,39 @@
+function notAvailable(){
+    alert('this option is not available at the moment');
+}
+
+function change() {
+    document.getElementById("js").style.backgroundColor = "black";
+    document.getElementById("js").style.color = "yellow";
+    document.getElementById("js").style.height="300px";
+    document.getElementById("js").style.width="300px";
+    document.getElementById("js").style.transition="1s";
+    document.getElementById("js").style.lineHeight="270px";
+}
+
+function myFunction() {
+    var x = document.getElementById("git").style.display="none"; 
+}
+
+function myFunction1() {
+    var x = document.getElementById("csharp").style.display="none"; 
+}
+
+function myFunction2() {
+    var x = document.getElementById("php").style.display="none"; 
+}
+function myFunction3() {
+    var x = document.getElementById("sql").style.display="none"; 
+}
+function myFunction4() {
+    var x = document.getElementById("npm").style.display="none"; 
+}
+function myFunction5() {
+    var x = document.getElementById("mvc").style.display="none"; 
+}
+function myFunction6() {
+    var x = document.getElementById("jquery").style.display="none"; 
+}
+function myFunction7() {
+    var x = document.getElementById("bs").style.display="none"; 
+}
